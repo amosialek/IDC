eb61ed043f18bd82260b0b5a350e6165fa0dc4c9
Michał Zakrzewski <miczak96@gmail.com>
2018-06-09 13:54:38 +0200

    Travis conf update

--- src/test/java/tai/IdcApplicationTests.java
+++ src/test/java/tai/IdcApplicationTests.java
@@ -2,11 +2,8 @@ package tai;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class IdcApplicationTests {
 
