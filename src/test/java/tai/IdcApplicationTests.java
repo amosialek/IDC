accc89e21e6e3e24ca9a4d031275aef3f992d7a1
Michał Zakrzewski <miczak96@gmail.com>
2018-06-09 13:46:04 +0200

    Mock test

--- src/test/java/tai/IdcApplicationTests.java
+++ src/test/java/tai/IdcApplicationTests.java
@@ -1,5 +1,6 @@
 package tai;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class IdcApplicationTests {
 
     @Test
     public void contextLoads() {
+        Assert.assertTrue(true);
     }
 
 }
